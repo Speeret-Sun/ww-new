b1ee265f356e698dc38a51dfa22a3b85090b012b
Speeret <speeretsun@gmail.com>
2025-02-22 00:29:39 +0300

    Add files via upload

--- j.js
+++ j.js
@@ -1,4 +1,4 @@
-document.getElementById('vz').addEventListener('click', function() {
+document.getElementById('vz1').addEventListener('click', function() {
     const vidsms = document.getElementById('vidsms');
     
     vidsms.style.display = 'flex'; 
@@ -6,3 +6,80 @@ document.getElementById('vz').addEventListener('click', function() {
         vidsms.style.opacity = 1; 
     }, 10); 
 });
+
+document.getElementById('vz2').addEventListener('click', function() {
+    const vidsms = document.getElementById('need');
+    
+    vidsms.style.display = 'flex'; 
+    setTimeout(() => {
+        vidsms.style.opacity = 1; 
+    }, 10); 
+});
+
+document.getElementById('vz2').addEventListener('click', function() {
+    const vidsms = document.getElementById('vz3');
+    
+    vidsms.style.display = 'flex'; 
+    setTimeout(() => {
+        vidsms.style.opacity = 1; 
+    }, 10); 
+});
+
+document.getElementById('vz2').addEventListener('click', function() {
+    const vidsms = document.getElementById('butt3');
+    
+    vidsms.style.display = 'flex'; 
+    setTimeout(() => {
+        vidsms.style.opacity = 1; 
+    }, 10); 
+});
+
+document.getElementById('vz3').addEventListener('click', function() {
+    const vidsms = document.getElementById('s1');
+    
+    vidsms.style.display = 'flex'; 
+    setTimeout(() => {
+        vidsms.style.opacity = 1; 
+    }, 10); 
+});
+
+document.getElementById('vz3').addEventListener('click', function() {
+    const vidsms = document.getElementById('s2');
+
+    setTimeout(() => {
+        vidsms.style.display = 'flex'; 
+
+        // Небольшая задержка для корректного отображения анимации
+        setTimeout(() => {
+            vidsms.style.opacity = 1; 
+        }, 10);
+    }, 3000); // Задержка в 3 секунды (3000 миллисекунд)
+});
+
+document.getElementById('vz3').addEventListener('click', function() {
+    const vidsms = document.getElementById('s3');
+
+    setTimeout(() => {
+        vidsms.style.display = 'flex'; 
+
+        // Небольшая задержка для корректного отображения анимации
+        setTimeout(() => {
+            vidsms.style.opacity = 1; 
+        }, 10);
+    }, 6000); // Задержка в 3 секунды (3000 миллисекунд)
+});
+
+
+document.getElementById('vz3').addEventListener('click', function() {
+    const vidsms = document.getElementById('s4');
+
+    setTimeout(() => {
+        vidsms.style.display = 'flex'; 
+
+        
+        setTimeout(() => {
+            vidsms.style.opacity = 1; 
+        }, 10);
+    }, 9000); 
+});
+
